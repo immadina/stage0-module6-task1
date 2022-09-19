c0e926dded5eb080d4bc8938a6ab491b13cf6440
madina <91madina@gmail.com>
2022-09-19 23:22:39 +0600

    Module 6, task 1

--- src/main/java/com/epam/mjc/stage0/Animal.java
+++ src/main/java/com/epam/mjc/stage0/Animal.java
@@ -2,4 +2,29 @@ package com.epam.mjc.stage0;
 
 public class Animal {
 
+    //Provide it with 3 private fields - color(String), numberOfPaws(int), hasFur(boolean).
+    String color = "Black";
+    int numberOfPaws = 4;
+    boolean hasFur = true;
+
+    //Add constructor with full parameters. Save the parameter order and names as it is listed in a second paragraph.
+    public Animal(String color, int numberOfPaws, boolean hasFur) {
+        this.color = color;
+        this.numberOfPaws = numberOfPaws;
+        this.hasFur = hasFur;
+    }
+
+    //Add a methods getDescription(), witch would use class fields and return a string with such pattern
+    // "This animal is mostly (color). It has (numberOfPaws) paws and ('a'/'no' -> depends on value of hasFur) fur."
+    public String getDescription() {
+        String fur = "no";
+        if (hasFur) {
+            fur = "a";
+        }
+        String paw = "paw";
+        if (numberOfPaws > 1) {
+            paw = "paws";
+        }
+        return "This animal is mostly " + color + ". It has " + numberOfPaws + " " + paw + " and " + fur + " fur.";
+    }
 }
--- src/main/java/com/epam/mjc/stage0/Bird.java
+++ src/main/java/com/epam/mjc/stage0/Bird.java
@@ -1,5 +1,21 @@
 package com.epam.mjc.stage0;
 
-public class Bird {
+public class Bird extends Animal {
+    public Bird() {
+        super("blue", 2, false);
+    }
 
+    @Override
+    public String getDescription() {
+        String fur = "no";
+        if (hasFur) {
+            fur = "a";
+        }
+        String paw = "paw";
+        if (numberOfPaws > 1) {
+            paw = "paws";
+        }
+        return "This animal is mostly " + color + ". It has "
+                + numberOfPaws + " " + paw + " and " + fur + " fur." + " Moreover, it has 2 wings and can fly.";
+    }
 }
